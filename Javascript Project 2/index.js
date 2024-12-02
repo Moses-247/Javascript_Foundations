fc4ff115c1a90b270300ab500bf6384143c2fb3e
Moses <asugam9@gmail.com>
2024-12-02 19:40:02 +0300

    java

---
+++ Javascript Project 2/index.js
@@ -0,0 +1,17 @@
+//String reversal
+
+function stringreverse(str){
+    return str.split('').reverse().join('');
+}
+
+//Count Characters
+
+function countChar(str){
+    return str.length;
+}
+
+//Capitalize Words
+
+function capitalWOrds(str){
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slide(1)).join(' ');
+}
