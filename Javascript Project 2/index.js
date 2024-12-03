03f4ad4f2dc17f089c0a8c2dab6a3319ed35e4b6
Moses <asugam9@gmail.com>
2024-12-03 20:49:33 +0300

    fib

--- Javascript Project 2/index.js
+++ Javascript Project 2/index.js
@@ -22,32 +22,32 @@ function findMax(arr) {
 }
 
 // Example usage
-const numbers = [3, 9, 5, 1, 7];
-console.log(findMax(numbers));
+// const numbers = [3, 9, 5, 1, 7];
+// console.log(findMax(numbers));
 
 
 function findMin(arr) {
     return Math.min(...arr);
 }
 //example
-const num = [2,6,3,8,4];
-console.log(findMin(num));
+// const num = [2,6,3,8,4];
+// console.log(findMin(num));
 
 //Sum 
 function sumArray(arr) {
     return arr.reduce((sum, num) => sum + num, 0);
 }
-const number = [3,6,3,13,8];
-console.log(sumArray(number));
+// const number = [3,6,3,13,8];
+// console.log(sumArray(number));
 
 // Filter 
 function filterArray(arr, condition) {
     return arr.filter(condition);
 }
-const numb = [3,6,8,7,1];
-const isEven = numb => numb % 2 === 0;
-const filternum = filterArray(numb,isEven);
-console.log(filternum);
+// const numb = [3,6,8,7,1];
+// const isEven = numb => numb % 2 === 0;
+// const filternum = filterArray(numb,isEven);
+// console.log(filternum);
 
 
 //Factorial
@@ -55,6 +55,9 @@ function factor(n) {
     if (n === 0) return 1;
     return n * factor(n - 1);
 }
+// let number = 5;
+// let result = factor(number);
+// console.log( `The factorial of the ${number} is ${result}`);
 
 
 
@@ -67,6 +70,11 @@ function isPrime(num) {
     return true;
 }
 
+// let number = 17;
+// let isPrimeNum =isPrime(number);
+
+// console.log(`${number} is prime: ${isPrimeNum}`);
+
 
 //Fibonacci
 function fibonacci(num) {
@@ -76,3 +84,8 @@ function fibonacci(num) {
     }
     return fib;
 }
+
+// let num = 10;
+// let fibSeq = fibonacci(num);
+
+// console.log(fibSeq);
